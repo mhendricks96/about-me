bb5df316af4545017a5a1ce4e894aa42ea39293f
mhendricks96 <mhendricks96@yahoo.com>
2021-01-12 23:30:15 -0800

    added second question

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ var username = prompt('What is your name?');
 alert('hello ' + username + ' nice to meet you');
 // write 5 questions. they must accept y or n OR yes or no in  ANY CASE
 
-var questionOne = prompt('Do I live in Washington state?').toLowerCase();
+var questionOne = prompt('Do you think I live in Washington state?').toLowerCase();
 //or
 //var normalizedQuestionOne = questionOne.toLowerCase();
 
@@ -20,3 +20,13 @@ if(questionOne === 'yes' || questionOne === 'y'){
   alert('ummmm...weird answer');
 }
 
+var questionTwo = prompt('Do you think I have a dog?').toLowerCase();
+
+if(questionTwo === 'yes' || questionTwo === 'y'){
+  //console.log('Yea! Of course I do! I have a wonderful old boy named Eazy-E');
+  alert('Yea! Of course I do! I have a wonderful old boy named Eazy-E');
+} else if(questionTwo === 'no' || questionTwo === 'n'){
+  alert('What do you think I am, a monster? Just so you know, I have a dog named Eazy-E');
+} else {
+  alert('are you paying attention to the questions?');
+}
