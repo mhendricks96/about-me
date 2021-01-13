0ff22aa92a3fd1a39f506da33c779d24cbd61fca
mhendricks96 <mhendricks96@yahoo.com>
2021-01-13 00:20:35 -0800

    cleaned up formatting

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-'use strict'
-console.log('hello world')
+'use strict';
+console.log('hello world');
 
 
 //get user name and offer greeting
@@ -11,10 +11,10 @@ var questionOne = prompt('Do you think I live in Washington state?').toLowerCase
 //or
 //var normalizedQuestionOne = questionOne.toLowerCase();
 
-if(questionOne === 'yes' || questionOne === 'y'){
+if (questionOne === 'yes' || questionOne === 'y') {
   //console.log('Nope, one state lower');
   alert('Nope, one state lower');
-} else if(questionOne === 'no' || questionOne === 'n'){
+} else if (questionOne === 'no' || questionOne === 'n') {
   alert('Good guess');
 } else {
   alert('ummmm...weird answer');
@@ -22,10 +22,10 @@ if(questionOne === 'yes' || questionOne === 'y'){
 
 var questionTwo = prompt('Do you think I have a dog?').toLowerCase();
 
-if(questionTwo === 'yes' || questionTwo === 'y'){
+if (questionTwo === 'yes' || questionTwo === 'y') {
   //console.log('Yea! Of course I do! I have a wonderful old boy named Eazy-E');
   alert('Yea! Of course I do! I have a wonderful old boy named Eazy-E');
-} else if(questionTwo === 'no' || questionTwo === 'n'){
+} else if (questionTwo === 'no' || questionTwo === 'n') {
   alert('What do you think I am, a monster? Just so you know, I have a dog named Eazy-E');
 } else {
   alert('are you paying attention to the questions?');
@@ -33,10 +33,10 @@ if(questionTwo === 'yes' || questionTwo === 'y'){
 
 var questionThree = prompt('Do you think Eazy is a good boy?').toLowerCase();
 
-if(questionThree === 'yes' || questionThree === 'y'){
+if (questionThree === 'yes' || questionThree === 'y') {
   //console.log('What insight! He really is. You must really know dogs!');
   alert('What insight! He really is. You must really know dogs!');
-} else if(questionThree === 'no' || questionThree === 'n'){
+} else if (questionThree === 'no' || questionThree === 'n') {
   alert('Well, you must be alot of fun at parties. Why would you say that? Eazy is a very good boy!');
 } else {
   alert('Ok, you definitely are not paying attention');
@@ -44,10 +44,10 @@ if(questionThree === 'yes' || questionThree === 'y'){
 
 var questionFour = prompt('Do you think I have any kids?').toLowerCase();
 
-if(questionFour === 'yes' || questionFour === 'y'){
+if (questionFour === 'yes' || questionFour === 'y') {
   //console.log('Nope, no kids for me');
   alert('Nope, no kids for me');
-} else if(questionFour === 'n' || questionFour === 'no'){
+} else if (questionFour === 'n' || questionFour === 'no') {
   alert('Very good.');
 } else {
   alert('looks like someone is just pressing random buttons');
@@ -55,10 +55,10 @@ if(questionFour === 'yes' || questionFour === 'y'){
 
 var questionFive = prompt('Ok, lets see if you have been paying attention. Do I have more children than dogs?').toLowerCase();
 
-if(questionFive === 'yes' || questionFive === 'y'){
+if (questionFive === 'yes' || questionFive === 'y') {
   //console.log('No, no, no, no. you must not be too great at math, because I know you wouldnt just skip through my questions');
   alert('No, no, no, no. you must not be too great at math, ' + username + ' because I know you wouldnt just skip through my questions');
-} else if( questionFive === 'no' || questionFive === 'n'){
+} else if (questionFive === 'no' || questionFive === 'n') {
   alert('Wonderful, ' + username + '! You have an attention span of at least 4 questions!');
 } else {
   alert(username + ' , you are hopeless');
