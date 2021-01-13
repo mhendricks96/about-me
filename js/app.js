d0aeafc0f40519110044c782e66937373fed7033
mhendricks96 <mhendricks96@yahoo.com>
2021-01-12 23:14:22 -0800

    added first question

--- js/app.js
+++ js/app.js
@@ -1,23 +1,22 @@
 'use strict'
 console.log('hello world')
 
-var banana = 42;
 
-if(banana){
-  console.log(banana);
-}
 //get user name and offer greeting
-var username = prompt('whats your name?');
+var username = prompt('What is your name?');
 alert('hello ' + username + ' nice to meet you');
 // write 5 questions. they must accept y or n OR yes or no in  ANY CASE
 
-var questionOne = prompt('Do i live in Portland?').toLowerCase();
+var questionOne = prompt('Do I live in Washington state?').toLowerCase();
 //or
 //var normalizedQuestionOne = questionOne.toLowerCase();
 
 if(questionOne === 'yes' || questionOne === 'y'){
-  //console.log('you are correct');
-  alert('you are correct');
+  //console.log('Nope, one state lower');
+  alert('Nope, one state lower');
+} else if(questionOne === 'no' || questionOne === 'n'){
+  alert('Good guess');
 } else {
-  alert('no way');
+  alert('ummmm...weird answer');
 }
+
